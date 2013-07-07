d33d334a24ea9e5f78e79726ccb69fb2e09925a6
Levi Smolin <levj220@gmail.com>
2013-07-07 16:24:29 -0500

    Quadtree structure

--- deepspace-ubuntu/QuadTree.h
+++ deepspace-ubuntu/QuadTree.h
@@ -8,6 +8,10 @@
 #ifndef QUADTREE_H_
 #define QUADTREE_H_
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 class QuadTree
 {
 public:
@@ -19,6 +23,15 @@ public:
 	QuadTree *ll;
 	QuadTree *lr;
 
+	QuadTree *N;
+	QuadTree *S;
+	QuadTree *E;
+	QuadTree *W;
+
+	glm::vec3 ne;
+	glm::vec3 nw;
+	glm::vec3 se;
+	glm::vec3 sw;
 
 };
 
